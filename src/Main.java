da4b79a2261f53775867e37cbccc3a25c4f98c20
Fariha Wali <farihawali@Farihas-Air.Starry-inc.net>
2023-03-29 20:39:07 -0400

    I have successfully resolved the issue and the code is ready to be committed to sun branch

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
        solar.sun ="1";
        solar.moon="2";
        solar.planets="2";
+
+       featureSun Xron= new featureSun();
+       Xron.temperature="123485093804";
+       Xron.radius= "38083403842";
+       Xron.color="pink";
     }
 }
